2e9229927affe3e335b934820d92422a02129d89
KrackKonk <colrino_cooper@hotmail.com>
2023-02-02 20:43:06 -0500

    $ git config --global user.name "John Doe" $ git config --global user.email johndoe@example.com

---
+++ Website/auth.py
@@ -0,0 +1,44 @@
+from flask import Blueprint , render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"  
+@auth.route('/sign-up' , methods=['GET','POST'])
+def sign_up():
+    if request.method == 'POST':
+        email= request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email)< 4:
+            flash ('Email must be greater than 4 characters.', catergory = 'error' )
+        elif len(firstName) <2:
+            flash ('First name must be greater than 2 characters.', catergory = 'error' )
+        elif password1 !=password2:
+            flash ('Password must match. ', catergory = 'error' )
+        elif len(password1) <7:
+            flash ('Password must be greater than 7 characters.', catergory = 'error' )
+        else:
+            new_user = User(email= email, firstName= firstName,  password= generate_password_hash (password1,  method='sha256'))
+
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', catergory='success')
+            return redirect(url_for('views.home'))
+
+
+
+
+  
+
+
+    return render_template("signup.html") 
